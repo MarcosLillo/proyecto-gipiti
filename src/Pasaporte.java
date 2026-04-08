309b207a562eba2989b2f4eeafa1293609a66ab1
MarcosLillo <eazywrld2.0@gmail.com>
2026-04-07 22:25:39 -0400

    clase pasaporte casi terminada

---
+++ src/Pasaporte.java
@@ -0,0 +1,32 @@
+//Marcos Lillo
+public class Pasaporte {
+    //atributos
+    private String numero;
+    private String nacionalidad;
+    //constructor
+    private Pasaporte(String numero, String nacionalidad) {
+        this.numero = numero;
+        this.nacionalidad = nacionalidad;
+    }
+    //getters
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    @Override
+    public String toString() {
+        return this.numero+ " " +this.nacionalidad;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        Pasaporte o = (Pasaporte) obj;
+        return this.numero.equals(o.numero) && this.nacionalidad.equals(o.nacionalidad);
+
+    }
+    //no ocupar of todavia
+}
