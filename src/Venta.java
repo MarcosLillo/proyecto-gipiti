2a749f7c25ecfab4744d67e18e5641f1adb311df
MarcosLillo <eazywrld2.0@gmail.com>
2026-04-23 22:59:28 -0400

    Avance en la clase venta, aun no terminada

--- src/Venta.java
+++ src/Venta.java
@@ -5,28 +5,44 @@ public class Venta {
     private  String idDocumento;
     private TipoDocumento tipo;
     private LocalDate fecha;
+    private Cliente cli;
 
     public Venta(String id, TipoDocumento tipo, LocalDate fecha, Cliente cli) {
-        this.id = id;
+        this.idDocumento = id;
         this.tipo = tipo;
         this.fecha = fecha;
         this.cli = cli;
     }
 
     public String getIdDocumento() {
+
         return idDocumento;
     }
 
     public TipoDocumento getTipo() {
+
         return tipo;
     }
 
     public LocalDate getFecha() {
+
         return fecha;
     }
     public Cliente getCliente(){
-        return cliente;
+        return cli;
 
     }
+    public Pasaje[] getPasajes() {
+        return pasajes.toArray(new Pasaje[0]);
+    }
+    public void createPasaje(int asiento, Viaje viaje, Pasajero pasajero){
+        Pasaje pasaje = new Pasaje(asiento, viaje, pasajero, this);
+        pasajes.add(pasaje);
+    }
+
+    public int getMonto() {
+
+    }
+
 
 }
