cdf36e6ee974c53103749d81711e91f9488c083f
IOnfinit <joaquin.castro2401@alumnos.ubiobio.cl>
2026-04-26 00:10:21 -0400

    Guardando / Me psicosio un error de String relacionado al jdk

---
+++ src/Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+
+    private String email;
+
+    public Cliente(IdPersona id, Nombre nom, String email) {
+
+        this.email = email;
+
+    }
+
+    public String getEmail() {
+
+        return email;
+
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+
+    }
+
+    public void addVenta(Venta venta) {
+
+        this.venta = venta;
+
+    }
+
+    public Venta[] getVentas() {
+
+        return ventas;
+
+    }
+
+}
