5769bac2837b246132ae899dec587705ecd8912e
maxisandsand123-dot <maxisandsand123@gmail.com>
2026-04-26 21:43:13 -0400

    Merge remote-tracking branch 'origin/Maxi' into Maxi
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml

---
+++ src/Rut.java
@@ -0,0 +1,23 @@
+
+public class Rut {
+    private int numero;
+    private char dv;
+
+    private Rut(int numero, char dv) {
+
+        this.numero = numero;
+        this.dv = dv;
+    }
+
+    public int getNumero() {
+
+        return numero;
+    }
+
+    public char getDv() {
+        return dv;
+    }
+    //no nos han pasado el of
+
+
+}
