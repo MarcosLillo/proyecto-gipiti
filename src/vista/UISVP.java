42666b84f25bd758af2beddcb3089278084c9bf0
IOnfinit <joaquin.castro2401@alumnos.ubiobio.cl>
2026-05-23 20:46:09 -0400

    Algo del UISVP

--- src/vista/UISVP.java
+++ src/vista/UISVP.java
@@ -1,5 +1,6 @@
 package vista;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //Singleton
@@ -21,4 +22,88 @@ public class UISVP {
         }
         return instance;
     }
+
+    //Lo que se vera como menu so...
+    public void menu() {
+        boolean salir = false;
+
+        while (salir != true) { //(!salir)
+
+            System.out.println("===============================================");
+            System.out.println("          ...::: Menú principal :::...         ");
+            System.out.println("1)  Crear empresa");
+            System.out.println("2)  Contratar tripulante");
+            System.out.println("3)  Crear terminal");
+            System.out.println("4)  Crear cliente");
+            System.out.println("5)  Crear bus");
+            System.out.println("6)  Crear viaje");
+            System.out.println("7)  Vender pasajes");
+            System.out.println("8)  Listar ventas");
+            System.out.println("9)  Listar viajes");
+            System.out.println("10) Listar pasajeros de viaje");
+            System.out.println("11) Listar empresas");
+            System.out.println("12) Listar llegadas/salidas de terminal");
+            System.out.println("13) Listar ventas de empresa");
+            System.out.println("14) Salir");
+            System.out.println("------------------------------------------------");
+            System.out.print("..:: Ingrese número de opción: ");
+            int opcion;
+            try {
+                opcion = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Opcion elegida invalida, ingresela nuevamente: ");
+                opcion = sc.nextInt();
+               //sc.nextLine(); //buffer, problemas de los next"Int"
+            }
+
+            switch (opcion) {
+                case 1:
+                    createEmpresa();
+                    break;
+                case 2:
+                    contrataTripulante();
+                    break;
+                case 3:
+                    createTerminal();
+                    break;
+                case 4:
+                    createCliente();
+                    break;
+                case 5:
+                    createBus();
+                    break;
+                case 6:
+                    createViaje();
+                    break;
+                case 7:
+                    vendePasajes();
+                    break;
+                case 8:
+                    listVentas();
+                    break;
+                case 9:
+                    listViajes();
+                    break;
+                case 10:
+                    listPasajerosViaje();
+                    break;
+                case 11:
+                    listEmpresas();
+                    break;
+                case 12:
+                    listLlegadasSalidasTerminal();
+                    break;
+                case 13:
+                    listVentasEmpresa();
+                    break;
+                case 14:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("????");
+            }
+        }
+    }
+
+
 }
