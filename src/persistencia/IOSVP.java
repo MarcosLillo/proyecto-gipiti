524e479bff7163eff1e85c3e6c6d76ce68946fa2
IOnfinit <joaquin.castro2401@alumnos.ubiobio.cl>
2026-06-20 17:02:15 -0400

    Pequeño avance

--- src/persistencia/IOSVP.java
+++ src/persistencia/IOSVP.java
@@ -4,6 +4,9 @@ import excepciones.SVPException;
 import java.io.*;
 import modelo.*; //Llama todo
 import utilidades.*;
+
+import javax.imageio.event.IIOWriteProgressListener;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,7 +28,8 @@ public class IOSVP {
     }
 
     //UML
-    public Object[] readDatosIniciales() {
+    public Object[] readDatosIniciales() throws SVPException {
+
         /*Lee el contenido del archivo de texto SVPDatosIniciales.txt y
         construye los objetos correspondientes de la capa modelo,
         incluyendo las asociaciones requeridas, si estas no se crean
@@ -33,21 +37,69 @@ public class IOSVP {
         invocados. Retorna un arreglo con todos los objetos creados. Lanza
         SVPException cuando no es posible abrir y/o leer el archivo
         asociando a la excepción un mensaje con el problema ocurrido*/
+
         File archivo = new File("SVPDatosIniciales.txt");
+        ArrayList<Object> objectList = new ArrayList<>();
+
+        //Inicializar los objetos, falta incluir mas weas ptm
+        Auxiliar aux = null;
+        Bus bus = null;
+        Cliente cliente = null;
+        Conductor conductor = null;
+        Empresa empresa = null;
+        Pago pago = null;
+        Pasaje pasaje = null;
+        Pasajero pasajero = null;
+        Persona persona = null;
+        Terminal terminal = null;
+        Tripulante tripulante = null;
+        Venta venta = null;
+        Viaje viaje = null;
+
+        //Me centro mejor en lo demas por ahora
     }
 
-    public void saveControladores(Object controladores) {
-        /*Almacena los objetos que recibe como parámetro en un archivo de
-        objetos llamado SVPObjetos.obj. Si no es posible abrir/crear el
-        archivo o no se puede grabar por problemas en el dispositivo lanza
-        SVPException indicando problema concreto*/
+    public void saveControladores(Object controladores) throws SVPException {
+
+        try {
+            File archivo = new File("SVPObjetos.obj"); //Referencia del archivo en la memoria
+            FileOutputStream fileOutput = new FileOutputStream(archivo); //Prepara el escribir datos en el archivo, si algo ya exoste se sobrescribe
+            ObjectOutputStream objOutput = new ObjectOutputStream(fileOutput); //La serializacion, guarda el estado del objeto
+
+            objOutput.writeObject(controladores); //Envia el objeto controlador
+
+            objOutput.close();
+            fileOutput.close();
+
+        } catch (FileNotFoundException e) { //FileOutputStream
+            throw new SVPException("Ah ocurrido un error al crear el archivo SVPObjetos.obj");
+        } catch (IOException e) { //ObjectOutputStream
+            throw new SVPException("No se puede guardar en el archivo SVPObjetos.obj");
+        }
     }
 
-    public Object[] readControladores() {
-        /*Retorna un arreglo con los objetos que lee desde el archivo
-        SVPObjetos.obj. Si no es posible abrir el archivo o leer los objetos
-        almacenados en él, lanza SVPException indicando el problema
-        concreto*/
+    public Object[] readControladores() throws SVPException {
+
+        try {
+            File archivo = new File("SVPObjetos.obj");
+            FileInputStream input = new FileInputStream(archivo);
+            ObjectInputStream objInput = new ObjectInputStream(input);
+
+            Object[] obj = (Object[])objInput.readObject();
+
+            if (obj.length == 0) {
+                System.out.println("No se pudo leer los datos");
+            }
+
+            return obj;
+
+        } catch (FileNotFoundException e) {
+            throw new SVPException("Ah ocurrido un error al crear el archivo SVPObjetos.obj");
+        } catch (IOException e) {
+            throw new SVPException("No se puede leer el archivo SVPObject.obj");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void savePasajesDeVentas(Pasaje[] pasajes, String nombreArchivo) {
